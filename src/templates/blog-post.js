035e4faf981b4bc699bb4dfa61bf8aa400dce15d
J. Aaron Eaton <aaron@channeleaton.com>
2017-10-31 22:17:05 -0500

    Add category display to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,37 +4,42 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 
 import Bio from '../components/Bio'
-import { rhythm, scale } from '../utils/typography'
+import {rhythm, scale} from '../utils/typography'
 
 class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
+	render() {
+		const post      = this.props.data.markdownRemark
+		const siteTitle = get(this.props, 'data.site.siteMetadata.title');
 
-    return (
-      <div>
-        <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
-      </div>
-    )
-  }
+		return (
+			<div>
+				<Helmet title={`${post.frontmatter.title} | ${siteTitle}`}/>
+				<h1>{post.frontmatter.title}</h1>
+				<p
+					style={{
+						...scale(-1 / 5),
+						display     : 'block',
+						marginBottom: rhythm(1),
+						marginTop   : rhythm(-1),
+					}}
+				>
+					{post.frontmatter.date}
+				</p>
+				<div dangerouslySetInnerHTML={{__html: post.html}}/>
+				<p className="post-topic"
+					style={{
+						fontSize: "0.9em",
+						color   : "#ababab"
+					}}>Topic: {post.frontmatter.category}</p>
+				<hr
+					style={{
+						marginBottom: rhythm(1),
+					}}
+				/>
+				<Bio/>
+			</div>
+		)
+	}
 }
 
 export default BlogPostTemplate
@@ -53,7 +58,8 @@ export const pageQuery = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        category
       }
     }
   }
-`
+`;
