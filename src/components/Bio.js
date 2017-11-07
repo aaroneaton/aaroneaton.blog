452346cf61a98fac661802824aab25cb05158bf9
J. Aaron Eaton <aaron@channeleaton.com>
2017-11-07 15:15:38 -0600

    eslint & formatting

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,34 +8,34 @@ import profilePic from './profile-pic.png'
 import { rhythm } from '../utils/typography'
 
 class Bio extends React.Component {
-  render() {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          marginBottom: rhythm(2.5),
-        }}
-      >
-        <img
-          src={profilePic}
-          alt={`J. Aaron Eaton`}
-          style={{
-            marginRight: rhythm(1 / 2),
-            marginBottom: 0,
-            width: rhythm(2),
-            height: rhythm(2),
-          }}
-        />
-        <p>
-          Hi! I'm <strong>J. Aaron Eaton</strong>. I help create amazing web experiences.
-	        <br />
-          <a href="https://twitter.com/aaroneaton">
-            You should follow me on Twitter
-          </a>
-        </p>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div
+				style={{
+					display: 'flex',
+					marginBottom: rhythm(2.5),
+				}}
+			>
+				<img
+					src={profilePic}
+					alt="J. Aaron Eaton"
+					style={{
+						marginRight: rhythm(1 / 2),
+						marginBottom: 0,
+						width: rhythm(2),
+						height: rhythm(2),
+					}}
+				/>
+				<p>
+					Hi! My name is <strong>J. Aaron Eaton</strong>. I help
+					create amazing web experiences.{' '}
+					<a href="https://twitter.com/aaroneaton">
+						You should follow me on Twitter
+					</a>
+				</p>
+			</div>
+		)
+	}
 }
 
 export default Bio
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,59 +1,53 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import { Container } from 'react-responsive-grid'
-
-import { rhythm, scale } from '../utils/typography'
 import 'prismjs/themes/prism-okaidia.css'
+import { rhythm, scale } from '../utils/typography'
 
-/** @TODO: Set up category template & query **/
+/** @TODO: Set up category template & query * */
 class Template extends React.Component {
-  render() {
-    const { location, children } = this.props
-    let header
+	render() {
+		const { children } = this.props
 
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
+		// const rootPath = (typeof __PREFIX_PATHS__ !== 'undefined' && __PREFIX_PATHS__) ? `${__PREFIX_PATHS__}/` : '/';
 
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            J. Aaron Eaton
-          </Link>
-        </h1>
-      );
-    return (
-      <Container
-        style={{
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        {header}
-        {children()}
-      </Container>
-    )
-  }
+		const header = (
+			<h1
+				style={{
+					...scale(1.5),
+					marginBottom: rhythm(1.5),
+					marginTop: 0,
+				}}
+			>
+				<Link
+					style={{
+						boxShadow: 'none',
+						textDecoration: 'none',
+						color: 'inherit',
+					}}
+					to="/"
+				>
+					J. Aaron Eaton
+				</Link>
+			</h1>
+		)
+		return (
+			<Container
+				style={{
+					maxWidth: rhythm(24),
+					padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+				}}
+			>
+				{header}
+				{children()}
+			</Container>
+		)
+	}
 }
 
 Template.propTypes = {
-  children: React.PropTypes.func,
-  location: React.PropTypes.object,
-  route: React.PropTypes.object,
+	children: PropTypes.func.isRequired,
 }
 
 export default Template
