492fca2e873b943366ef2d65fc40250d2ad1697e
J. Aaron Eaton <aaron@channeleaton.com>
2017-11-07 15:52:25 -0600

    Fix 128px favicon name

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ class BlogIndex extends React.Component {
 				<Helmet title={siteTitle}>
 					<link rel="icon" type="image/png" href={'./favicon.png'} sizes="16x16" />
 					<link rel="icon" type="image/png" href={'./favicon-32x32.png'} sizes="32x32" />
-					<link rel="icon" type="image/png" href={"./favicon-128.png"} sizes="128x128" />
+					<link rel="icon" type="image/png" href={"./favicon-128x128.png"} sizes="128x128" />
 					<link rel="icon" type="image/png" href={"./favicon-96x96.png"} sizes="96x96" />
 					<link rel="icon" type="image/png" href={"./favicon-196x196.png"} sizes="196x196" />
 				</Helmet>
