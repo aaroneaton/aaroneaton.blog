6f1bbe85051c74d763621d5033b55a83ed692fb0
J. Aaron Eaton <aaron@channeleaton.com>
2017-11-08 12:04:21 -0600

    Try importing favicons

---
+++ src/components/favicons/index.js
@@ -0,0 +1,7 @@
+import favicon16 from './favicon.png';
+import favicon32 from './favicon-32x32.png';
+import favicon96 from './favicon-96x96.png';
+import favicon128 from './favicon-128x128.png';
+import favicon196 from './favicon-196x196.png';
+
+export {favicon16, favicon32, favicon96, favicon128, favicon196}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Helmet from 'react-helmet';
 
 import Bio from '../components/Bio';
 import { rhythm } from '../utils/typography';
+import * as favicons from '../components/favicons';
 
 class BlogIndex extends React.Component {
 	render() {
@@ -14,11 +15,11 @@ class BlogIndex extends React.Component {
 		return (
 			<div>
 				<Helmet title={siteTitle}>
-					<link rel="icon" type="image/png" href={'./favicon.png'} sizes="16x16" />
-					<link rel="icon" type="image/png" href={'./favicon-32x32.png'} sizes="32x32" />
-					<link rel="icon" type="image/png" href={"./favicon-128x128.png"} sizes="128x128" />
-					<link rel="icon" type="image/png" href={"./favicon-96x96.png"} sizes="96x96" />
-					<link rel="icon" type="image/png" href={"./favicon-196x196.png"} sizes="196x196" />
+					<link rel="icon" type="image/png" href={favicons.favicon16} sizes="16x16" />
+					<link rel="icon" type="image/png" href={favicons.favicon32} sizes="32x32" />
+					<link rel="icon" type="image/png" href={favicons.favicon96} sizes="96x96" />
+					<link rel="icon" type="image/png" href={favicons.favicon128} sizes="128x128" />
+					<link rel="icon" type="image/png" href={favicons.favicon196} sizes="196x196" />
 				</Helmet>
 				<Bio />
 				{posts.map(post => {
